50741c6ab3bd1aac12bad4acef811f268722886d
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-11-15 12:46:00 +0300

    nav componenti olustuşturuldu

---
+++ components/header/nav/index.jsx
@@ -0,0 +1,45 @@
+export default function Nav() {
+  return (
+    <>
+      <ul class="nav-list">
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <span class="icon">🏠</span>
+            <span class="label">Ana Sayfa</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <span class="icon">👥</span>
+            <span class="label">Ağım</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <span class="icon">💼</span>
+            <span class="label">İş İlanları</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <span class="icon">✉️</span>
+            <span class="label">Mesajlaşma</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <span class="icon notification">🔔</span>
+            <span class="badge">3</span>
+            <span class="label">Bildirimler</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <span class="icon proafile">👤</span>
+            <span class="label">Ben</span>
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+}
