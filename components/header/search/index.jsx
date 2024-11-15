af1daf7f4186efa815b6fdcc30e6b4775988f402
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-11-15 12:46:15 +0300

    seacrh componenti olusturuldu

---
+++ components/header/search/index.jsx
@@ -0,0 +1,8 @@
+export default function Search() {
+  return (
+    <div className="search-container">
+      <input type="search" placeholder="Arama yap" />
+      <span class="search-icon">🔍</span>
+    </div>
+  );
+}
