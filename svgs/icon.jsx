c22286a89b46df45acded604bea7aa019e818391
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-11-17 02:05:51 +0300

    svgs ler eklendi

---
+++ svgs/icon.jsx
@@ -0,0 +1,20 @@
+export default function Icon() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        aria-hidden="true"
+        data-supported-dps="16x16"
+        viewBox="0 0 16 16"
+        data-token-id="385"
+        width="24"
+        height="24"
+        className="_1v88awfd cnuthtag _1ptbkx61c _1ptbkx614g"
+        aria-label=""
+      >
+        <path d="M2 2h2.67v2.67H2zm4.67 2.67h2.67V2H6.67zM11.34 2v2.67h2.67V2zM2 9.33h2.67V6.66H2zm4.67 0h2.67V6.66H6.67zm4.67 0h2.67V6.66h-2.67zM2 14h2.67v-2.67H2zm4.67 0h2.67v-2.67H6.67zm4.67 0h2.67v-2.67h-2.67z"></path>
+      </svg>
+    </>
+  );
+}
---
+++ svgs/okIcon.jsx
@@ -0,0 +1,19 @@
+export default function OkIcon() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        aria-hidden="true"
+        data-supported-dps="16x16"
+        viewBox="0 0 16 16"
+        data-token-id="379"
+        width="16"
+        height="16"
+        className=""
+      >
+        <path d="M8 11 3 6h10Z"></path>
+      </svg>
+    </>
+  );
+}
