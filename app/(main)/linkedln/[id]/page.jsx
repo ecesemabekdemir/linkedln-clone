ea28c14bad7bbc1c78bfc0d6575dac784187fd62
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-11-17 02:03:16 +0300

    page ve detay a gitmesi için olusturdum

---
+++ app/(main)/linkedln/[id]/page.jsx
@@ -0,0 +1,7 @@
+export default function DetailPage() {
+  return (
+    <>
+      <h1>detay sayfası</h1>
+    </>
+  );
+}
---
+++ app/(main)/page.jsx
@@ -0,0 +1,11 @@
+import Header from "@/components/header";
+import ProfilePage from "@/components/profilePage";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Header />
+      <ProfilePage />
+    </div>
+  );
+}
