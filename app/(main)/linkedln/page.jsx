ba4ae338e19270d430a1549a80db942048eb99fc
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-11-18 20:06:19 +0300

    header de ufak css değişikliği yaptım ve bildirim için css hazırladım

--- app/(main)/linkedln/page.jsx
+++ app/(main)/linkedln/page.jsx
@@ -1,8 +1,10 @@
+import Header from "@/components/header";
 import ProfilePage from "@/components/profilePage";
 
 export default function UserProfile() {
   return (
     <>
+      <Header />
       <ProfilePage />
     </>
   );
