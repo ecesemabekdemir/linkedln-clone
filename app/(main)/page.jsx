8697077170858559cb039d8df04c1bc21f8018f9
Sema Bekdemir <ecesemadimen96@gmail.com>
2025-01-08 17:48:50 +0300

    mainde userControl çağrıldı

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,13 +1,14 @@
-import Header from "@/components/header";
 import PostCreator from "@/components/postCreator";
 import ProfileSidebar from "@/components/profileSidebar";
 import PostDetailPage from "./posts/[id]/page";
 import NewAdsSidebar from "@/components/newAdsSidebar";
+import { UserControl } from "@/helpers/usercontrol";
 
 export default async function Home() {
   return (
     <>
       <div className="container">
+        <UserControl />
         <ProfileSidebar />
         <div className="content-container">
           <PostCreator />
