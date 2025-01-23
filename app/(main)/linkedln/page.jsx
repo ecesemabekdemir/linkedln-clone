6c576718aae7c0a279763b8a8d3e92ef32fc6662
Sema Bekdemir <ecesemadimen96@gmail.com>
2025-01-23 17:39:51 +0300

    footer kaldırıldı

--- app/(main)/linkedln/page.jsx
+++ app/(main)/linkedln/page.jsx
@@ -1,7 +1,6 @@
 import ProfilePage from "@/components/profilePage";
 import SaveUser from "@/components/save-users";
 import "./linked.css";
-import Footer from "@/components/footer";
 
 export default async function UserProfile() {
   return (
